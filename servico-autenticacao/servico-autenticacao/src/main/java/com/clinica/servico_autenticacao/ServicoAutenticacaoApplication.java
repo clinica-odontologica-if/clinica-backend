48da7948ea5f9f48735a7d9b66b45c5f8ed5956e
Estella1 <estella.santos.moreira@gmail.com>
2026-05-17 20:09:51 -0300

    Estrutura nova do projeto

---
+++ servico-autenticacao/servico-autenticacao/src/main/java/com/clinica/servico_autenticacao/ServicoAutenticacaoApplication.java
@@ -0,0 +1,13 @@
+package com.clinica.servico_autenticacao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicoAutenticacaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServicoAutenticacaoApplication.class, args);
+	}
+
+}
---
+++ servico-autenticacao/servico-autenticacao/src/test/java/com/clinica/servico_autenticacao/ServicoAutenticacaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clinica.servico_autenticacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicoAutenticacaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
