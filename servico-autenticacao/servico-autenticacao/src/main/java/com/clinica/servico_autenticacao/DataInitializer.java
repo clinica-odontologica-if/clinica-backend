d3344a5cacdaf0e081822b1185099dc48f9ec751
Estella1 <estella.santos.moreira@gmail.com>
2026-05-29 16:19:09 -0300

    feat: Alaterando senhas login e senha de inicialização para versão mais segura para

--- servico-autenticacao/servico-autenticacao/src/main/java/com/clinica/servico_autenticacao/DataInitializer.java
+++ servico-autenticacao/servico-autenticacao/src/main/java/com/clinica/servico_autenticacao/DataInitializer.java
@@ -4,6 +4,7 @@ import com.clinica.servico_autenticacao.model.Role;
 import com.clinica.servico_autenticacao.model.Usuario;
 import com.clinica.servico_autenticacao.repository.UsuarioRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
@@ -15,14 +16,25 @@ public class DataInitializer implements CommandLineRunner {
     private final UsuarioRepository usuarioRepository;
     private final PasswordEncoder passwordEncoder;
 
+    @Value("${admin.senha.gerente}")
+    private String senhaGerente;
+
+    @Value("${admin.senha.atendente}")
+    private String senhaAtendente;
+
+    @Value("${admin.senha.dentista}")
+    private String senhaDentista;
+
+    @Value("${admin.senha.auxiliar}")
+    private String senhaAuxiliar;
+
     @Override
     public void run(String... args) {
         if (usuarioRepository.count() == 0) {
-            usuarioRepository.save(new Usuario(null, "Gerente", "gerente@clinica.com", passwordEncoder.encode("123456"), Role.GERENTE));
-            usuarioRepository.save(new Usuario(null, "Atendente", "atendente@clinica.com", passwordEncoder.encode("123456"), Role.ATENDENTE));
-            usuarioRepository.save(new Usuario(null, "Dentista", "dentista@clinica.com", passwordEncoder.encode("123456"), Role.DENTISTA));
-            usuarioRepository.save(new Usuario(null, "Auxiliar", "auxiliar@clinica.com", passwordEncoder.encode("123456"), Role.AUXILIAR));
-            System.out.println("Usuarios de teste criados!");
+            usuarioRepository.save(new Usuario(null, "Gerente", "gerente@clinica.com", passwordEncoder.encode(senhaGerente), Role.GERENTE));
+            usuarioRepository.save(new Usuario(null, "Atendente", "atendente@clinica.com", passwordEncoder.encode(senhaAtendente), Role.ATENDENTE));
+            usuarioRepository.save(new Usuario(null, "Dentista", "dentista@clinica.com", passwordEncoder.encode(senhaDentista), Role.DENTISTA));
+            usuarioRepository.save(new Usuario(null, "Auxiliar", "auxiliar@clinica.com", passwordEncoder.encode(senhaAuxiliar), Role.AUXILIAR));
         }
     }
 }
