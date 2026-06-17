c3336cd78cbd675fbac5d787ef51eb756ebfe0da
Estella1 <estella.santos.moreira@gmail.com>
2026-06-17 16:31:31 -0300

    "feat: teste automatizado no dicovery-server"

--- discovery-server/discovery-server/src/test/java/com/clinica/discovery_server/DiscoveryServerApplicationTests.java
+++ discovery-server/discovery-server/src/test/java/com/clinica/discovery_server/DiscoveryServerApplicationTests.java
@@ -1,9 +1,12 @@
 package com.clinica.discovery_server;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.netflix.eureka.server.EurekaServerAutoConfiguration;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
+@EnableAutoConfiguration(exclude = { EurekaServerAutoConfiguration.class })
 class DiscoveryServerApplicationTests {
 
 	@Test
