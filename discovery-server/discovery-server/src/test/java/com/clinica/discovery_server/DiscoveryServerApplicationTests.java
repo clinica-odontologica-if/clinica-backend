2ccdfd9065d1d3492291a9c558d5c221d781cabe
Estella1 <estella.santos.moreira@gmail.com>
2026-06-21 22:33:05 -0300

    fix: correções nos testes

--- discovery-server/discovery-server/src/test/java/com/clinica/discovery_server/DiscoveryServerApplicationTests.java
+++ discovery-server/discovery-server/src/test/java/com/clinica/discovery_server/DiscoveryServerApplicationTests.java
@@ -5,7 +5,13 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.cloud.netflix.eureka.server.EurekaServerAutoConfiguration;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
+@SpringBootTest(
+		webEnvironment = SpringBootTest.WebEnvironment.NONE,
+		properties = {
+				"eureka.client.enabled=false",
+				"spring.cloud.service-registry.auto-registration.enabled=false"
+		}
+)
 @EnableAutoConfiguration(exclude = { EurekaServerAutoConfiguration.class })
 class DiscoveryServerApplicationTests {
 
