3bc324220727023cf040c3ddfaa4e9a00377fd1b
Vitinh0liveira <0082301@academico.ifmg.edu.br>
2026-06-22 23:04:03 -0300

    feat: estrutura inicial do servico-paciente via Spring Initializr

---
+++ servico-paciente/servico-paciente/src/main/java/com/clinica/servico_paciente/ServicoPacienteApplication.java
@@ -0,0 +1,13 @@
+package com.clinica.servico_paciente;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicoPacienteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServicoPacienteApplication.class, args);
+	}
+
+}
---
+++ servico-paciente/servico-paciente/src/test/java/com/clinica/servico_paciente/ServicoPacienteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clinica.servico_paciente;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicoPacienteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
