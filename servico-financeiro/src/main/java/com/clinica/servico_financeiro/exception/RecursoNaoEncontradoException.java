a4cf7bb4e930d12713c8ccaac5819e50d6b15c89
Estella1 <estella.santos.moreira@gmail.com>
2026-07-01 16:53:38 -0300

    feat: cria base do servico financeiro

---
+++ servico-financeiro/src/main/java/com/clinica/servico_financeiro/exception/RecursoNaoEncontradoException.java
@@ -0,0 +1,8 @@
+package com.clinica.servico_financeiro.exception;
+
+public class RecursoNaoEncontradoException extends RuntimeException {
+
+    public RecursoNaoEncontradoException(String message) {
+        super(message);
+    }
+}
